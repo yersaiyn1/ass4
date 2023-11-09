736409b21ee1847f3f2bfcf450c75968c5578f2f
Yersaiyn <yersaiyn@MacBook-Pro.local>
2023-11-09 14:55:00 +0600

    v1.0

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('newsletter_submit').onclick = function() {
+    alert("You've subscribed :)");
+}
+document.getElementById('milestone_counter_id1').innerHTML = Math.floor(Math.random() * 100);
+document.getElementById('milestone_counter_id2').innerHTML = Math.floor(Math.random() * 100);
+document.getElementById('milestone_counter_id3').innerHTML = Math.floor(Math.random() * 100);
+document.getElementById('milestone_counter_id4').innerHTML = Math.floor(Math.random() * 100);
